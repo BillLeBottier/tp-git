484eb1e1401c7b5b57665f46ce3c4423395a2b88
RobinVassaux <robin.vassaux@fer.hr>
2024-04-12 15:00:51 +0200

    test pytest

---
+++ test_calculator.py
@@ -0,0 +1,19 @@
+from calculator import addition
+from calculator import substraction
+from calculator import division
+
+
+def test_addition():
+    assert addition(1, 2) == 3
+
+def test_substraction():
+    assert substraction(3, 2) == 1
+
+
+def test_division():
+    assert division(2, 2) == 1   
+
+
+
+
+
