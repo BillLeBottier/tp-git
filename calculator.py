63c2cfbcb1208d34ce40cc39cb2afdfa44d1cc53
RobinVassaux <robin.vassaux@fer.hr>
2024-04-12 15:25:33 +0200

    class

--- calculator.py
+++ calculator.py
@@ -1,14 +1,17 @@
-def addition(a,b):
-    result = a+b 
-    return result
+class calculator:
+    def addition(self,a,b):
+        result = a+b 
+        return result
 
-def substraction(a,b):
-    result = a-b 
-    return result
+    def substraction(self,a,b):
+        result = a-b
+        return result 
+    
 
-def division(a,b):
-    result = a/b
-    return result
+    def division(self,a,b):
+        result = a/b
+        return result
+    
 
-
-#test
+calculator = calculator()
+print(calculator.addition(2,3))
